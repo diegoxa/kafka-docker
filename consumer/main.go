020c6495dc657e94c2ca1af5d43aa0f2e4f2dba5
Diego Andrade <diego@kargo.com>
2018-07-20 17:06:19 -0400

    consumer example

---
+++ consumer/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+var bootstrapServers = "10.122.0.108:29092"
+
+func main() {
+	// library librdkafka is needed
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": bootstrapServers,
+		"group.id":          "group1",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.SubscribeTopics([]string{"topic"}, nil)
+	defer c.Close()
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else {
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+			break
+		}
+	}
+}
